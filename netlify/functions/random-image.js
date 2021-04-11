64b82b2bd3ec7bad72990994add3b6054e7f712d
Kurt Stephens <>
2021-04-11 00:38:20 +0000

    Added unsplash function

--- netlify/functions/random-image.js
+++ netlify/functions/random-image.js
@@ -1,11 +1,11 @@
 const fetch = require('node-fetch');
-
-const UNSPLASH_ACCESS_KEY='i1zKjeCtUmMWoUpz__Wgs7xufzVT8ajS7bFIUjjKvgo';
+const ACCESS_KEY = process.env.UNSPLASH_ACCESS_KEY;
 
 exports.handler = async function(event, context) {
   const url = 'https://api.unsplash.com/photos/random';
   const headers = {
-    'Authorization': `Client-ID ${UNSPLASH_ACCESS_KEY}`
+    'Authorization': `Client-ID ${ACCESS_KEY}`,
+    'Accept-Version': 'v1'
   };
 
   try {
@@ -13,7 +13,7 @@ exports.handler = async function(event, context) {
     const data = await response.json();
 
     return {
-      statusCode: 200,
+      statusCode: response.status,
       body: JSON.stringify(data)
     };
   } catch (error) {
