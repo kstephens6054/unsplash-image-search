e97737a17f5ea703a3ebf243f0e4be40adf3f58c
Kurt Stephens <>
2021-04-11 01:42:10 +0000

    Added routing to Unsplash function

---
+++ netlify/functions/unsplash/index.js
@@ -0,0 +1,37 @@
+const { Path } = require('path-parser');
+
+const { handler as photosHandler } = require('./photos');
+const { handler as searchHandler } = require('./search');
+
+const PATH_PREFIX = '/.netlify/functions/unsplash';
+
+const error404 = () => {
+  return {
+    statusCode: 404,
+    body: JSON.stringify({
+      error: 'Not Found'
+    })
+  }
+};
+
+exports.handler = async function(event, context) {
+  if (!event.path.startsWith(PATH_PREFIX)) {
+    return error404();
+  }
+
+  const localEvent = {
+    event,
+    path: event.path.replace(PATH_PREFIX, ''),
+    accessKey: process.env.UNSPLASH_ACCESS_KEY;
+  };
+
+  if (localEvent.path.startsWith('/photos')) {
+    return photosHandler(localEvent, context);
+  }
+
+  if (localEvent.path.startsWith('/search')) {
+    return searchHandler(localEvent, context);
+  }
+
+  return error404();
+};
